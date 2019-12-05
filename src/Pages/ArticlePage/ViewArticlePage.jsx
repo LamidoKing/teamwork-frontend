e4d6bd309b2fcf7187a0fb43ae530c8888465ece
lamido <kinglamido@gmail.com>
2019-12-05 14:58:32 +0100

    ft(edit-own-article):add edit-own-article [finishes #10]

--- src/Pages/ArticlePage/ViewArticlePage.jsx
+++ src/Pages/ArticlePage/ViewArticlePage.jsx
@@ -4,8 +4,14 @@ import { connect } from "react-redux"
 import Moment from "react-moment"
 import withStyles from "@material-ui/core/styles/withStyles"
 import Delete from "@material-ui/icons/Delete"
+import Dialog from "@material-ui/core/Dialog"
+import DialogActions from "@material-ui/core/DialogActions"
+import DialogContent from "@material-ui/core/DialogContent"
+import DialogContentText from "@material-ui/core/DialogContentText"
+import DialogTitle from "@material-ui/core/DialogTitle"
 import Edit from "@material-ui/icons/Edit"
 import Skeleton from "@material-ui/lab/Skeleton"
+import CustomInput from "../../Components/CustomInput/CustomInput"
 import GridContainer from "../../Components/Grid/GridContainer"
 import GridItem from "../../Components/Grid/GridItem"
 import Button from "../../Components/CustomButtons/Button"
@@ -13,41 +19,159 @@ import Card from "../../Components/Card/Card"
 import CardBody from "../../Components/Card/CardBody"
 import CardFooter from "../../Components/Card/CardFooter"
 import feedStyle from "../../Style/Pages/feedStyle"
-import { ArticleAction } from "../../redux/actions"
+import { ArticleAction, GeneralAction } from "../../redux/actions"
+import { AuthToken } from "../../Utils"
 
 const propTypes = {
   classes: PropTypes.oneOfType([PropTypes.object]).isRequired,
   getArticle: PropTypes.func.isRequired,
+  getInputData: PropTypes.func.isRequired,
+  editArticle: PropTypes.func.isRequired,
   articleData: PropTypes.oneOfType([PropTypes.object]),
+  editData: PropTypes.oneOfType([PropTypes.object]),
   match: PropTypes.oneOfType([PropTypes.object]).isRequired
 }
 
 const defaultProps = {
-  articleData: {}
+  articleData: {},
+  editData: {}
 }
 
 const mapStateToProps = state => ({
   articleData: state.article.specificArticleData.data || {},
+  editData: state.article.editedArticleData || {},
   error: state.article.error || {}
 })
 
 const mapActionCreators = {
-  getArticle: ArticleAction.getArticle
+  getArticle: ArticleAction.getArticle,
+  getInputData: GeneralAction.getInputData,
+  editArticle: ArticleAction.editArticle
 }
 
 class ViewArticlePage extends React.PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      open: false
+    }
+  }
+
   componentDidMount() {
     const { getArticle, match } = this.props
     const { id } = match.params
     getArticle(id)
   }
 
+  componentDidUpdate(prevProps) {
+    const { editData, getArticle, match } = this.props
+    const { id } = match.params
+
+    if (prevProps.editData !== editData) {
+      getArticle(id)
+    }
+  }
+
+  handleInput = (name, value) => {
+    const { getInputData } = this.props
+
+    getInputData({
+      key: name,
+      value
+    })
+  }
+
+  handleClickOpen = () => {
+    const { articleData } = this.props
+    this.setState({
+      open: true,
+      CurrentTitle: articleData.title,
+      CurrentArticle: articleData.article
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    })
+  }
+
+  handleChange = name => ({ target: { value } }) => {
+    this.setState({
+      [name]: value
+    })
+
+    this.handleInput(name, value)
+  }
+
+  handleSubmit = () => {
+    const { editArticle, articleData } = this.props
+    const { id } = articleData
+    editArticle(id)
+    this.handleClose()
+  }
+
   render() {
     const { classes, articleData } = this.props
-    const { article, title, createdOn } = articleData
+    const { article, title, createdOn, authorId } = articleData
+    const { open, CurrentTitle, CurrentArticle } = this.state
+    const author = AuthToken.getConfirm().userId === authorId
 
     return (
       <div>
+        <div>
+          <Dialog
+            open={open}
+            onClose={this.handleClose}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+            <DialogContent>
+              <DialogContentText>You Can Edit Your Article</DialogContentText>
+              <CustomInput
+                labelText="Article Title"
+                id="articleTitle"
+                name="articleTitle"
+                onChange={this.handleInput}
+                formControlProps={{
+                  fullWidth: true
+                }}
+                inputProps={{
+                  value: CurrentTitle,
+                  type: "text",
+                  name: "articleTitle",
+                  onChange: this.handleChange("CurrentTitle")
+                }}
+              />
+              <CustomInput
+                labelText="Article Title"
+                id="CurrentArticle"
+                name="CurrentArticle"
+                onChange={this.handleInput}
+                formControlProps={{
+                  fullWidth: true
+                }}
+                inputProps={{
+                  type: "text",
+                  value: CurrentArticle,
+                  name: "article",
+                  onChange: this.handleChange("CurrentArticle"),
+                  multiline: true,
+                  rows: 8
+                }}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={this.handleSubmit} color="primary">
+                EDIT
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
         <GridContainer justify="center">
           {article ? (
             <GridItem xs={12} sm={12} md={12}>
@@ -60,36 +184,39 @@ class ViewArticlePage extends React.PureComponent {
                   <div className={classes.date}>
                     <Moment fromNow>{createdOn}</Moment>
                   </div>
-                  <div>
-                    <Button
-                      color="primary"
-                      round
-                      className={classes.marginRight}
-                    >
-                      <Edit className={classes.icons} /> EDIT
-                    </Button>
-                    <Button
-                      color="primary"
-                      round
-                      className={classes.marginRight}
-                    >
-                      <Delete className={classes.icons} /> DELETE
-                    </Button>
-                  </div>
+                  {author && (
+                    <div>
+                      <Button
+                        color="primary"
+                        round
+                        className={classes.marginRight}
+                        onClick={this.handleClickOpen}
+                      >
+                        <Edit className={classes.icons} /> EDIT
+                      </Button>
+                      <Button
+                        color="primary"
+                        round
+                        className={classes.marginRight}
+                      >
+                        <Delete className={classes.icons} /> DELETE
+                      </Button>
+                    </div>
+                  )}
                 </CardFooter>
               </Card>
             </GridItem>
           ) : (
             <GridItem xs={12} sm={12} md={12}>
               <Skeleton variant="rect" height={300}>
-                <Skeleton height={10} width="25%" className={classes.center} />
-                <Skeleton height={10} style={{ marginBottom: 8 }} />
-                <Skeleton height={10} style={{ marginBottom: 8 }} />
-                <Skeleton height={10} style={{ marginBottom: 8 }} />
-                <Skeleton height={10} style={{ marginBottom: 8 }} />
-                <Skeleton height={10} style={{ marginBottom: 8 }} />
-                <Skeleton height={10} style={{ marginBottom: 8 }} />
-                <Skeleton height={10} width="80%" />
+                <Skeleton height={18} width="25%" className={classes.center} />
+                <Skeleton height={18} style={{ marginBottom: 8 }} />
+                <Skeleton height={18} style={{ marginBottom: 8 }} />
+                <Skeleton height={18} style={{ marginBottom: 8 }} />
+                <Skeleton height={18} style={{ marginBottom: 8 }} />
+                <Skeleton height={18} style={{ marginBottom: 8 }} />
+                <Skeleton height={18} style={{ marginBottom: 8 }} />
+                <Skeleton height={18} width="80%" />
               </Skeleton>
             </GridItem>
           )}
--- src/redux/reducers/article.js
+++ src/redux/reducers/article.js
@@ -42,14 +42,37 @@ const handleSpecificArticleData = (state, action) => {
   })
 }
 
+const handleEditedArticleData = (state, action) => {
+  if (action.payload.status === "success") {
+    return update(state, {
+      editedArticleData: {
+        $set: action.payload
+      },
+      error: {
+        $set: {}
+      }
+    })
+  }
+  return update(state, {
+    error: {
+      $set: action.payload
+    },
+    editedArticleData: {
+      $set: {}
+    }
+  })
+}
+
 const ACTION_HANDLERS = {
   POST_ARTICLE: handlePostArticle,
-  ARTICLE_DATA: handleSpecificArticleData
+  ARTICLE_DATA: handleSpecificArticleData,
+  EDITED_ARTICLE_DATA: handleEditedArticleData
 }
 
 const initialState = {
   articleData: {},
   specificArticleData: {},
+  editedArticleData: {},
   error: {}
 }
 
