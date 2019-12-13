d716931677a40047c419c8aaf2bd2715decf5507
lamido <kinglamido@gmail.com>
2019-12-14 00:17:48 +0100

    Updates

---
+++ precache-manifest.1328bc7dfb847e97c6d47b2a4ed2ad92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5111c82b5a793c2653b198fac198ac",
+    "url": "/teamwork-frontend/index.html"
+  },
+  {
+    "revision": "b9f310fc256de6db5c09",
+    "url": "/teamwork-frontend/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "1d49f014a82105453269",
+    "url": "/teamwork-frontend/static/css/main.24a95c43.chunk.css"
+  },
+  {
+    "revision": "b9f310fc256de6db5c09",
+    "url": "/teamwork-frontend/static/js/2.fe8f0151.chunk.js"
+  },
+  {
+    "revision": "1d49f014a82105453269",
+    "url": "/teamwork-frontend/static/js/main.7fe7247e.chunk.js"
+  },
+  {
+    "revision": "5087dad2b3c076dff790",
+    "url": "/teamwork-frontend/static/js/runtime-main.3982bfcd.js"
+  },
+  {
+    "revision": "ebe9884b2152a08a525fcd5054019532",
+    "url": "/teamwork-frontend/static/media/image_placeholder.ebe9884b.jpg"
+  },
+  {
+    "revision": "9d377b10ce778c4938b3c7e2c63a229a",
+    "url": "/teamwork-frontend/static/media/register.9d377b10.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.906aff0c13119ff9d6d5e8c18dc05139.js"
+  "/teamwork-frontend/precache-manifest.1328bc7dfb847e97c6d47b2a4ed2ad92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teamwork-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
