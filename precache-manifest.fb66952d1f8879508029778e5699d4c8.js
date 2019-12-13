5624a952c4cecbaccb61a14c910c247379c5347c
lamido <kinglamido@gmail.com>
2019-12-13 14:41:19 +0100

    Updates

---
+++ precache-manifest.fb66952d1f8879508029778e5699d4c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e528c0c76152ee0813a64a133bbee0b",
+    "url": "/LamidoKing/teamwork-frontend/index.html"
+  },
+  {
+    "revision": "b9f310fc256de6db5c09",
+    "url": "/LamidoKing/teamwork-frontend/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "f30b2b99f26fdc5564b3",
+    "url": "/LamidoKing/teamwork-frontend/static/css/main.24a95c43.chunk.css"
+  },
+  {
+    "revision": "b9f310fc256de6db5c09",
+    "url": "/LamidoKing/teamwork-frontend/static/js/2.fe8f0151.chunk.js"
+  },
+  {
+    "revision": "f30b2b99f26fdc5564b3",
+    "url": "/LamidoKing/teamwork-frontend/static/js/main.33d400c1.chunk.js"
+  },
+  {
+    "revision": "8d0b26e4dd1bceb580eb",
+    "url": "/LamidoKing/teamwork-frontend/static/js/runtime-main.4760cc8b.js"
+  },
+  {
+    "revision": "ebe9884b2152a08a525fcd5054019532",
+    "url": "/LamidoKing/teamwork-frontend/static/media/image_placeholder.ebe9884b.jpg"
+  },
+  {
+    "revision": "9d377b10ce778c4938b3c7e2c63a229a",
+    "url": "/LamidoKing/teamwork-frontend/static/media/register.9d377b10.jpeg"
+  }
+]);
